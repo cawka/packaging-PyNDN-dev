named-data/PyCCN

Add ability to create Name and ContentObject from raw bytes in Python
For example, when content object is prepared in advance, it should be
possible to create "parsed" ContentObject python object from raw bytes.

bytes() is necessary on Linux, so it provides "buffer" interface for the
data in the variable

Change-Id: I54dabcde0bfca40f6e7ba3e81403a4ab6634c08b

csrc/methods_contentobject.c
@@ -226,6 +226,43 @@ _pyccn_content_object_set_comps(PyObject *py_content_object,
 // Content Objects
 
 PyObject *
+ContentObject_obj_from_ccn_buffer (PyObject *py_buffer)
+{
+  int r;
+  Py_buffer buffer;
+  PyObject *py_o;
+  PyObject *py_data = NULL;
+  struct ccn_charbuf *data;
+
+  if (!PyObject_CheckBuffer (py_buffer))
+    {
+      PyErr_SetString(PyExc_TypeError, "The argument is not a buffer");
+      return NULL;
+    }
+
+  r = PyObject_GetBuffer (py_buffer, &buffer, PyBUF_SIMPLE);
+  if (r < 0)
+    {
+      PyErr_SetString(PyExc_TypeError, "The argument is not a buffer");
+      return NULL;
+    }
+
+  py_data = CCNObject_New_charbuf (CONTENT_OBJECT, &data);
+  JUMP_IF_NULL(py_data, error);
+  r = ccn_charbuf_append (data, buffer.buf, buffer.len);
+  JUMP_IF_NEG_MEM(r, error);
+
+  py_o = ContentObject_obj_from_ccn (py_data);
+  Py_CLEAR(py_data);
+  JUMP_IF_NULL(py_o, error);
+
+  return py_o;
+  
+error:
+  return NULL;
+}
+
+PyObject *
 ContentObject_obj_from_ccn(PyObject *py_content_object)
 {
 	struct ccn_charbuf *content_object;
@@ -491,6 +528,12 @@ PyObject *
 _pyccn_cmd_ContentObject_obj_from_ccn(PyObject *UNUSED(self), PyObject *py_co)
 {
 	return ContentObject_obj_from_ccn(py_co);
+}
+
+PyObject *
+_pyccn_cmd_ContentObject_obj_from_ccn_buffer(PyObject *UNUSED(self), PyObject *py_buffer)
+{
+	return ContentObject_obj_from_ccn_buffer(py_buffer);
 }
 
 PyObject *

csrc/methods_contentobject.h
@@ -17,14 +17,19 @@ struct ccn_indexbuf *_pyccn_content_object_get_comps(
 void _pyccn_content_object_set_comps(PyObject *py_content_object,
 		struct ccn_indexbuf *comps);
 PyObject *ContentObject_obj_from_ccn(PyObject *py_content_object);
+PyObject *ContentObject_obj_from_ccn_buffer (PyObject *py_buffer);
+
 PyObject *_pyccn_cmd_content_to_bytes(PyObject *self, PyObject *arg);
 PyObject *_pyccn_cmd_content_to_bytearray(PyObject *self, PyObject *arg);
 PyObject *_pyccn_cmd_encode_ContentObject(PyObject *self, PyObject *args);
 PyObject *_pyccn_cmd_ContentObject_obj_from_ccn(PyObject *self, PyObject *py_co);
+PyObject *_pyccn_cmd_ContentObject_obj_from_ccn_buffer(PyObject *self, PyObject *py_co);
 PyObject *_pyccn_cmd_digest_contentobject(PyObject *self, PyObject *args);
 PyObject *_pyccn_cmd_content_matches_interest(PyObject *self, PyObject *args);
 PyObject *_pyccn_cmd_verify_content(PyObject *self, PyObject *args);
 PyObject *_pyccn_cmd_verify_signature(PyObject *self, PyObject *args);
+
+
 
 #endif	/* MEDHODS_CONTENTOBJECT_H */
 

csrc/methods_name.c
@@ -190,6 +190,60 @@ _pyccn_cmd_name_comps_from_ccn(PyObject *UNUSED(self), PyObject *py_cname)
 }
 
 PyObject *
+_pyccn_cmd_name_comps_from_ccn_buffer (PyObject *UNUSED(self), PyObject *py_buffer)
+{
+  int r;
+  Py_buffer buffer;
+  PyObject *py_component_list = NULL, *py_component;
+
+  if (!PyObject_CheckBuffer (py_buffer))
+    {
+      PyErr_SetString(PyExc_TypeError, "The argument is not a buffer");
+      return NULL;
+    }
+
+  r = PyObject_GetBuffer (py_buffer, &buffer, PyBUF_SIMPLE);
+  if (r < 0)
+    {
+      PyErr_SetString(PyExc_TypeError, "The argument is not a buffer");
+      return NULL;
+    }
+
+  // Create component list
+  py_component_list = PyList_New (0);
+  JUMP_IF_NULL (py_component_list, error);
+  
+  {
+    struct ccn_indexbuf *idx = ccn_indexbuf_create ();
+    const struct ccn_charbuf namebuf = { buffer.len, buffer.len, (unsigned char *)buffer.buf };
+    ccn_name_split (&namebuf, idx);
+
+    const unsigned char *compPtr = NULL;
+    size_t size = 0;
+    int i = 0;
+    while (ccn_name_comp_get(namebuf.buf, idx, i, &compPtr, &size) == 0)
+      {
+        py_component = PyBytes_FromStringAndSize ((char *) compPtr, size);
+        JUMP_IF_NULL (py_component, error);
+
+        r = PyList_Append (py_component_list, py_component);
+        Py_DECREF (py_component);
+        JUMP_IF_NEG (r, error);
+
+        i++;
+      }
+    ccn_indexbuf_destroy(&idx);
+  }
+
+  return py_component_list;
+  
+ error:
+  Py_XDECREF (py_component_list);
+  return NULL;
+}
+
+
+PyObject *
 Name_obj_from_ccn(PyObject *py_cname)
 {
 	PyObject *py_Name = NULL, *py_kargs;

csrc/methods_name.h
@@ -13,6 +13,8 @@
 
 PyObject *_pyccn_cmd_name_comps_to_ccn(PyObject *self, PyObject *py_name_components);
 PyObject *_pyccn_cmd_name_comps_from_ccn(PyObject *self, PyObject *py_cname);
+PyObject *_pyccn_cmd_name_comps_from_ccn_buffer (PyObject *self, PyObject *py_buffer);
+
 PyObject *Name_obj_from_ccn(PyObject *ccn_data);
 PyObject *Name_obj_to_ccn(PyObject *py_name);
 PyObject *Name_from_ccn_tagged_bytearray(const unsigned char *buf,

python/pyccn/Name.py
@@ -15,15 +15,19 @@ NAME_NORMAL = 0
 NAME_ANY    = 1
 
 class Name(object):
-	def __init__(self, components=[], name_type=NAME_NORMAL, ccn_data=None):
+	def __init__(self, components=[], name_type=NAME_NORMAL, ccn_data=None, ccnb_buffer=None):
 		self._setattr('type', name_type)
 
 		# pyccn
 		#self._setattr('ccn_data_dirty', True)
 		self._setattr('ccn_data', ccn_data)
 
+                # Name from simple buffer containing name in ccnb encoding
+                if ccnb_buffer:
+                        self._setattr('components', _pyccn.name_comps_from_ccn_buffer (bytes (ccnb_buffer)))
+
 		# Name from CCN
-		if ccn_data:
+		elif ccn_data:
 			self._setattr('components', _pyccn.name_comps_from_ccn(ccn_data))
 			self._setattr('ccn_data_dirty', False)
 

cawka/packaging-PyNDN-dev

csrc/methods_contentobject.c
@@ -226,6 +226,43 @@ _pyndn_content_object_set_comps(PyObject *py_content_object,
 // Content Objects
 
 PyObject *
+ContentObject_obj_from_ndn_buffer (PyObject *py_buffer)
+{
+  int r;
+  Py_buffer buffer;
+  PyObject *py_o;
+  PyObject *py_data = NULL;
+  struct ndn_charbuf *data;
+
+  if (!PyObject_CheckBuffer (py_buffer))
+    {
+      PyErr_SetString(PyExc_TypeError, "The argument is not a buffer");
+      return NULL;
+    }
+
+  r = PyObject_GetBuffer (py_buffer, &buffer, PyBUF_SIMPLE);
+  if (r < 0)
+    {
+      PyErr_SetString(PyExc_TypeError, "The argument is not a buffer");
+      return NULL;
+    }
+
+  py_data = NDNObject_New_charbuf (CONTENT_OBJECT, &data);
+  JUMP_IF_NULL(py_data, error);
+  r = ndn_charbuf_append (data, buffer.buf, buffer.len);
+  JUMP_IF_NEG_MEM(r, error);
+
+  py_o = ContentObject_obj_from_ndn (py_data);
+  Py_CLEAR(py_data);
+  JUMP_IF_NULL(py_o, error);
+
+  return py_o;
+  
+error:
+  return NULL;
+}
+
+PyObject *
 ContentObject_obj_from_ndn(PyObject *py_content_object)
 {
 	struct ndn_charbuf *content_object;
@@ -491,6 +528,12 @@ PyObject *
 _pyndn_cmd_ContentObject_obj_from_ndn(PyObject *UNUSED(self), PyObject *py_co)
 {
 	return ContentObject_obj_from_ndn(py_co);
+}
+
+PyObject *
+_pyndn_cmd_ContentObject_obj_from_ndn_buffer(PyObject *UNUSED(self), PyObject *py_buffer)
+{
+	return ContentObject_obj_from_ndn_buffer(py_buffer);
 }
 
 PyObject *

csrc/methods_contentobject.h
@@ -17,14 +17,18 @@ struct ndn_indexbuf *_pyndn_content_object_get_comps(
 void _pyndn_content_object_set_comps(PyObject *py_content_object,
 		struct ndn_indexbuf *comps);
 PyObject *ContentObject_obj_from_ndn(PyObject *py_content_object);
+PyObject *ContentObject_obj_from_ndn_buffer (PyObject *py_buffer);
 PyObject *_pyndn_cmd_content_to_bytes(PyObject *self, PyObject *arg);
 PyObject *_pyndn_cmd_content_to_bytearray(PyObject *self, PyObject *arg);
 PyObject *_pyndn_cmd_encode_ContentObject(PyObject *self, PyObject *args);
 PyObject *_pyndn_cmd_ContentObject_obj_from_ndn(PyObject *self, PyObject *py_co);
+PyObject *_pyndn_cmd_ContentObject_obj_from_ndn_buffer(PyObject *self, PyObject *py_co);
 PyObject *_pyndn_cmd_digest_contentobject(PyObject *self, PyObject *args);
 PyObject *_pyndn_cmd_content_matches_interest(PyObject *self, PyObject *args);
 PyObject *_pyndn_cmd_verify_content(PyObject *self, PyObject *args);
 PyObject *_pyndn_cmd_verify_signature(PyObject *self, PyObject *args);
+
+
 
 #endif	/* MEDHODS_CONTENTOBJECT_H */
 

csrc/methods_name.c
@@ -190,6 +190,60 @@ _pyndn_cmd_name_comps_from_ndn(PyObject *UNUSED(self), PyObject *py_cname)
 }
 
 PyObject *
+_pyndn_cmd_name_comps_from_ndn_buffer (PyObject *UNUSED(self), PyObject *py_buffer)
+{
+  int r;
+  Py_buffer buffer;
+  PyObject *py_component_list = NULL, *py_component;
+
+  if (!PyObject_CheckBuffer (py_buffer))
+    {
+      PyErr_SetString(PyExc_TypeError, "The argument is not a buffer");
+      return NULL;
+    }
+
+  r = PyObject_GetBuffer (py_buffer, &buffer, PyBUF_SIMPLE);
+  if (r < 0)
+    {
+      PyErr_SetString(PyExc_TypeError, "The argument is not a buffer");
+      return NULL;
+    }
+
+  // Create component list
+  py_component_list = PyList_New (0);
+  JUMP_IF_NULL (py_component_list, error);
+  
+  {
+    struct ndn_indexbuf *idx = ndn_indexbuf_create ();
+    const struct ndn_charbuf namebuf = { buffer.len, buffer.len, (unsigned char *)buffer.buf };
+    ndn_name_split (&namebuf, idx);
+
+    const unsigned char *compPtr = NULL;
+    size_t size = 0;
+    int i = 0;
+    while (ndn_name_comp_get(namebuf.buf, idx, i, &compPtr, &size) == 0)
+      {
+        py_component = PyBytes_FromStringAndSize ((char *) compPtr, size);
+        JUMP_IF_NULL (py_component, error);
+
+        r = PyList_Append (py_component_list, py_component);
+        Py_DECREF (py_component);
+        JUMP_IF_NEG (r, error);
+
+        i++;
+      }
+    ndn_indexbuf_destroy(&idx);
+  }
+
+  return py_component_list;
+  
+ error:
+  Py_XDECREF (py_component_list);
+  return NULL;
+}
+
+
+PyObject *
 Name_obj_from_ndn(PyObject *py_cname)
 {
 	PyObject *py_Name = NULL, *py_kargs;

csrc/methods_name.h
@@ -13,6 +13,7 @@
 
 PyObject *_pyndn_cmd_name_comps_to_ndn(PyObject *self, PyObject *py_name_components);
 PyObject *_pyndn_cmd_name_comps_from_ndn(PyObject *self, PyObject *py_cname);
+PyObject *_pyndn_cmd_name_comps_from_ndn_buffer (PyObject *self, PyObject *py_buffer);
 PyObject *Name_obj_from_ndn(PyObject *ndn_data);
 PyObject *Name_obj_to_ndn(PyObject *py_name);
 PyObject *Name_from_ndn_tagged_bytearray(const unsigned char *buf,

python/pyndn/Name.py
@@ -15,15 +15,19 @@ NAME_NORMAL = 0
 NAME_ANY    = 1
 
 class Name(object):
-	def __init__(self, components=[], name_type=NAME_NORMAL, ndn_data=None):
+	def __init__(self, components=[], name_type=NAME_NORMAL, ndn_data=None, ndnb_buffer=None):
 		self._setattr('type', name_type)
 
 		# pyndn
 		#self._setattr('ndn_data_dirty', True)
 		self._setattr('ndn_data', ndn_data)
 
+                # Name from simple buffer containing name in ndnb encoding
+                if ndnb_buffer:
+                        self._setattr('components', _pyndn.name_comps_from_ndn_buffer (bytes (ndnb_buffer)))
+
 		# Name from NDN
-		if ndn_data:
+		elif ndn_data:
 			self._setattr('components', _pyndn.name_comps_from_ndn(ndn_data))
 			self._setattr('ndn_data_dirty', False)
 