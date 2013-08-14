named-data/PyCCN

Significant change with key management: in most cases there is no need (and it is actually incompatible with openssl command line tools) to work with RSA keys directly.  Instead, most of the calls converted to use EVP_PKEY* (aka struct ccn_pkey*) directly.
Change-Id: If05dfbd66e36d624a756a28c8a59a58c203875fe

csrc/key_utils.h
@@ -35,9 +35,9 @@ struct keypair {
 };
 
 void initialize_crypto(void);
-int create_public_key_digest(RSA *private_key_rsa,
+int create_public_key_digest(struct ccn_pkey*,
 		PyObject **py_public_key_digest, int *public_key_digest_len);
-int ccn_keypair_from_rsa(int public_only, RSA *private_key_rsa,
+int ccn_keypair(int public_only, struct ccn_pkey*,
 		PyObject **py_private_key_ccn,
 		PyObject **py_public_key_ccn);
 PyObject *_pyccn_privatekey_dup(const struct ccn_pkey *key);

csrc/methods_key.c
@@ -147,7 +147,6 @@ Key_obj_from_ccn(PyObject *py_key_ccn)
 {
 	struct ccn_pkey *key_ccn;
 	PyObject *py_obj_Key;
-	RSA *private_key_rsa = NULL;
 	PyObject *py_private_key_ccn = NULL, *py_public_key_ccn = NULL,
 			*py_public_key_digest = NULL;
 	int public_key_digest_len;
@@ -180,17 +179,13 @@ Key_obj_from_ccn(PyObject *py_key_ccn)
 	// it out to RSA
 	// Also, create the digest...
 	// These non-ccn functions assume the CCN defaults, RSA + SHA256
-	private_key_rsa = ccn_key_to_rsa(key_ccn);
-	JUMP_IF_NULL(private_key_rsa, error);
 
-	r = ccn_keypair_from_rsa(public_only, private_key_rsa, &py_private_key_ccn,
+	r = ccn_keypair(public_only, key_ccn, &py_private_key_ccn,
 			&py_public_key_ccn);
 	JUMP_IF_NEG(r, error);
 
-	r = create_public_key_digest(private_key_rsa, &py_public_key_digest,
+	r = create_public_key_digest(key_ccn, &py_public_key_digest,
 			&public_key_digest_len);
-	RSA_free(private_key_rsa);
-	private_key_rsa = NULL;
 	JUMP_IF_NEG(r, error);
 
 	//  ccn_digest has a more convoluted API, with examples
@@ -232,7 +227,6 @@ error:
 	Py_XDECREF(py_private_key_ccn);
 	Py_XDECREF(py_public_key_ccn);
 	Py_XDECREF(py_public_key_digest);
-	RSA_free(private_key_rsa);
 	Py_XDECREF(py_obj_Key);
 	return NULL;
 }

cawka/packaging-PyNDN-dev

csrc/key_utils.h
@@ -35,9 +35,9 @@ struct keypair {
 };
 
 void initialize_crypto(void);
-int create_public_key_digest(RSA *private_key_rsa,
+int create_public_key_digest(struct ndn_pkey*,
 		PyObject **py_public_key_digest, int *public_key_digest_len);
-int ndn_keypair_from_rsa(int public_only, RSA *private_key_rsa,
+int ndn_keypair(int public_only, struct ndn_pkey*,
 		PyObject **py_private_key_ndn,
 		PyObject **py_public_key_ndn);
 PyObject *_pyndn_privatekey_dup(const struct ndn_pkey *key);

csrc/methods_key.c
@@ -147,7 +147,6 @@ Key_obj_from_ndn(PyObject *py_key_ndn)
 {
 	struct ndn_pkey *key_ndn;
 	PyObject *py_obj_Key;
-	RSA *private_key_rsa = NULL;
 	PyObject *py_private_key_ndn = NULL, *py_public_key_ndn = NULL,
 			*py_public_key_digest = NULL;
 	int public_key_digest_len;
@@ -180,17 +179,13 @@ Key_obj_from_ndn(PyObject *py_key_ndn)
 	// it out to RSA
 	// Also, create the digest...
 	// These non-ndn functions assume the NDN defaults, RSA + SHA256
-	private_key_rsa = ndn_key_to_rsa(key_ndn);
-	JUMP_IF_NULL(private_key_rsa, error);
 
-	r = ndn_keypair_from_rsa(public_only, private_key_rsa, &py_private_key_ndn,
+	r = ndn_keypair(public_only, key_ndn, &py_private_key_ndn,
 			&py_public_key_ndn);
 	JUMP_IF_NEG(r, error);
 
-	r = create_public_key_digest(private_key_rsa, &py_public_key_digest,
+	r = create_public_key_digest(key_ndn, &py_public_key_digest,
 			&public_key_digest_len);
-	RSA_free(private_key_rsa);
-	private_key_rsa = NULL;
 	JUMP_IF_NEG(r, error);
 
 	//  ndn_digest has a more convoluted API, with examples
@@ -232,7 +227,6 @@ error:
 	Py_XDECREF(py_private_key_ndn);
 	Py_XDECREF(py_public_key_ndn);
 	Py_XDECREF(py_public_key_digest);
-	RSA_free(private_key_rsa);
 	Py_XDECREF(py_obj_Key);
 	return NULL;
 }