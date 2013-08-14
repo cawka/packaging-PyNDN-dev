named-data/PyCCN

Implementing ability to save private key in PEM format in encrypted form
Change-Id: I60f368ae909e4e49b40a500bad19bd4cdfd1bcc2

csrc/key_utils.c
@@ -330,17 +330,25 @@ generate_key(int length, PyObject **py_private_key_ccn,
 //
 
 int
-write_key_pem_private(FILE *fp, struct ccn_pkey *private_key_ccn)
+write_key_pem_private(FILE *fp, struct ccn_pkey *private_key_ccn, char *password)
 {
 	unsigned long err;
+        int res = 0;
 
-	if (!PEM_write_PrivateKey(fp, (EVP_PKEY *) private_key_ccn, NULL, NULL, 0, NULL, NULL)) {
-		err = ERR_get_error();
-		PyErr_Format(g_PyExc_CCNKeyError, "Unable to write Private Key: %s",
-				ERR_reason_error_string(err));
-		return -1;
+        if (password) {
+          res = PEM_write_PKCS8PrivateKey (fp, (EVP_PKEY *) private_key_ccn, EVP_aes_256_cbc (), password, strlen(password), NULL, NULL);
+        }
+        else {
+          res = PEM_write_PrivateKey(fp, (EVP_PKEY *) private_key_ccn, NULL, NULL, 0, NULL, NULL);
+        }
+        
+	if (!res) {
+          err = ERR_get_error();
+          PyErr_Format(g_PyExc_CCNKeyError, "Unable to write Private Key: %s",
+                       ERR_reason_error_string(err));
+          return -1;
 	}
-
+        
 	return 0;
 }
 
@@ -360,7 +368,7 @@ write_key_pem_public(FILE *fp, struct ccn_pkey *public_key_ccn)
 }
 
 PyObject *
-get_key_pem_private(const struct ccn_pkey *private_key_ccn)
+get_key_pem_private(const struct ccn_pkey *private_key_ccn, char *password)
 {
 	unsigned long err;
 	BIO *bio;
@@ -371,8 +379,12 @@ get_key_pem_private(const struct ccn_pkey *private_key_ccn)
 	bio = BIO_new(BIO_s_mem());
 	JUMP_IF_NULL(bio, openssl_error);
 
-	r = PEM_write_bio_PrivateKey(bio, (EVP_PKEY *) private_key_ccn, NULL, NULL, 0, NULL,
-			NULL);
+        if (password) {
+          r = PEM_write_bio_PKCS8PrivateKey (bio, (EVP_PKEY *) private_key_ccn, EVP_aes_256_cbc (), NULL, 0, NULL, password);
+        }
+        else {
+          r = PEM_write_bio_PrivateKey(bio, (EVP_PKEY *) private_key_ccn, NULL, NULL, 0, NULL, NULL);
+        }
 	if (!r)
 		goto openssl_error;
 
@@ -480,8 +492,9 @@ error:
 
 int
 read_key_pem(FILE *fp, PyObject **py_private_key_ccn,
-		PyObject **py_public_key_ccn, PyObject **py_public_key_digest,
-		int *public_key_digest_len)
+             PyObject **py_public_key_ccn, PyObject **py_public_key_digest,
+             int *public_key_digest_len,
+             char *password)
 {
         struct ccn_pkey *private_key = NULL;
 	PyObject *py_private_key = NULL, *py_public_key = NULL;
@@ -493,7 +506,7 @@ read_key_pem(FILE *fp, PyObject **py_private_key_ccn,
 	r = fgetpos(fp, &fpos);
 	JUMP_IF_NEG(r, errno_error);
 
-	private_key = (struct ccn_pkey *)PEM_read_PrivateKey(fp, NULL, NULL, NULL);
+        private_key = (struct ccn_pkey *)PEM_read_PrivateKey(fp, NULL, NULL, password);
 	if (private_key) {
 		public_only = 0;
 		goto success;
@@ -551,7 +564,8 @@ error:
 int
 put_key_pem(int is_public_only, PyObject *py_key_pem,
             PyObject **py_private_key_ccn, PyObject **py_public_key_ccn,
-            PyObject **py_public_key_digest)
+            PyObject **py_public_key_digest,
+            char *password)
 {
 	unsigned char *key_pem;
 	Py_ssize_t pem_len;
@@ -569,7 +583,7 @@ put_key_pem(int is_public_only, PyObject *py_key_pem,
 	if (is_public_only)
           key = (struct ccn_pkey*)PEM_read_bio_PUBKEY(bio, NULL, NULL, NULL);
 	else
-          key = (struct ccn_pkey*)PEM_read_bio_PrivateKey(bio, NULL, NULL, NULL);
+          key = (struct ccn_pkey*)PEM_read_bio_PrivateKey(bio, NULL, NULL, password);
 	JUMP_IF_NULL(key, openssl_error);
 
 	r = ccn_keypair(is_public_only, key, py_private_key_ccn, py_public_key_ccn);

csrc/key_utils.h
@@ -47,19 +47,21 @@ int generate_key(int length, PyObject **private_key_ccn,
 //int generate_keypair(int length, struct keypair** KP);
 
 // We use "PEM" to make things "readable" for now
-int write_key_pem_private(FILE *fp, struct ccn_pkey* private_key_ccn);
+int write_key_pem_private(FILE *fp, struct ccn_pkey* private_key_ccn, char *password);
 int write_key_pem_public(FILE *fp, struct ccn_pkey* private_key_ccn);
 
-PyObject *get_key_pem_private(const struct ccn_pkey *private_key_ccn);
+PyObject *get_key_pem_private(const struct ccn_pkey *private_key_ccn, char *password);
 PyObject *get_key_pem_public(const struct ccn_pkey *key_ccn);
 PyObject *get_key_der_private(struct ccn_pkey *private_key_ccn);
 PyObject *get_key_der_public(struct ccn_pkey *public_key_ccn);
 int read_key_pem(FILE *fp, PyObject **py_private_key_ccn,
-		PyObject **public_key_ccn, PyObject **py_public_key_digest,
-		int *public_key_digest_len);
+                 PyObject **public_key_ccn, PyObject **py_public_key_digest,
+                 int *public_key_digest_len,
+                 char *password);
 int put_key_pem(int is_public_only, PyObject *py_key_pem,
 		PyObject **py_private_key_ccn, PyObject **py_public_key_ccn,
-		PyObject **py_public_key_digest);
+		PyObject **py_public_key_digest,
+                char *password);
 int put_key_der(int is_public_only, PyObject *py_key_der,
 		PyObject **py_private_key_ccn, PyObject **py_public_key_ccn,
 		PyObject **py_public_key_digest, int *public_key_digest_len);

python/pyccn/Key.py
@@ -32,16 +32,16 @@ class Key(object):
 	def publicToDER(self):
 		return _pyccn.DER_write_key(self.ccn_data_public)
 
-	def privateToPEM(self, filename = None):
+	def privateToPEM(self, filename = None, password = None):
 		if not self.ccn_data_private:
 			raise _pyccn.CCNKeyError("Key is not private")
 
 		if filename:
 			f = open(filename, 'w')
-			_pyccn.PEM_write_key(self.ccn_data_private, file=f)
+			_pyccn.PEM_write_key(self.ccn_data_private, file=f, password = password)
 			f.close()
 		else:
-			return _pyccn.PEM_write_key(self.ccn_data_private)
+			return _pyccn.PEM_write_key(self.ccn_data_private, password = password)
 
 	def publicToPEM(self, filename = None):
 		if filename:
@@ -61,15 +61,15 @@ class Key(object):
 				_pyccn.DER_read_key(public=public)
 			return
 
-	def fromPEM(self, filename = None, private = None, public = None):
+	def fromPEM(self, filename = None, private = None, public = None, password = None):
 		if filename:
 			f = open(filename, 'r')
 			(self.ccn_data_private, self.ccn_data_public, self.publicKeyID) = \
-				_pyccn.PEM_read_key(file=f)
+				_pyccn.PEM_read_key(file=f, password = password)
 			f.close()
 		elif private:
 			(self.ccn_data_private, self.ccn_data_public, self.publicKeyID) = \
-				_pyccn.PEM_read_key(private=private)
+				_pyccn.PEM_read_key(private=private, password = password)
 		elif public:
 			(self.ccn_data_private, self.ccn_data_public, self.publicKeyID) = \
 				_pyccn.PEM_read_key(public=public)
@@ -81,9 +81,9 @@ class Key(object):
                 return key
 
         @staticmethod
-        def createFromPEM (filename = None, private = None, public = None):
+        def createFromPEM (filename = None, private = None, public = None, password = None):
                 key = Key ()
-                key.fromPEM (filename, private, public)
+                key.fromPEM (filename, private, public, password)
                 return key
 
 # plus library helper functions to generate and serialize keys?

cawka/packaging-PyNDN-dev

csrc/key_utils.c
@@ -330,17 +330,25 @@ generate_key(int length, PyObject **py_private_key_ndn,
 //
 
 int
-write_key_pem_private(FILE *fp, struct ndn_pkey *private_key_ndn)
+write_key_pem_private(FILE *fp, struct ndn_pkey *private_key_ndn, char *password)
 {
 	unsigned long err;
+        int res = 0;
 
-	if (!PEM_write_PrivateKey(fp, (EVP_PKEY *) private_key_ndn, NULL, NULL, 0, NULL, NULL)) {
-		err = ERR_get_error();
-		PyErr_Format(g_PyExc_NDNKeyError, "Unable to write Private Key: %s",
-				ERR_reason_error_string(err));
-		return -1;
+        if (password) {
+          res = PEM_write_PKCS8PrivateKey (fp, (EVP_PKEY *) private_key_ndn, EVP_aes_256_cbc (), password, strlen(password), NULL, NULL);
+        }
+        else {
+          res = PEM_write_PrivateKey(fp, (EVP_PKEY *) private_key_ndn, NULL, NULL, 0, NULL, NULL);
+        }
+        
+	if (!res) {
+          err = ERR_get_error();
+          PyErr_Format(g_PyExc_NDNKeyError, "Unable to write Private Key: %s",
+                       ERR_reason_error_string(err));
+          return -1;
 	}
-
+        
 	return 0;
 }
 
@@ -360,7 +368,7 @@ write_key_pem_public(FILE *fp, struct ndn_pkey *public_key_ndn)
 }
 
 PyObject *
-get_key_pem_private(const struct ndn_pkey *private_key_ndn)
+get_key_pem_private(const struct ndn_pkey *private_key_ndn, char *password)
 {
 	unsigned long err;
 	BIO *bio;
@@ -371,8 +379,12 @@ get_key_pem_private(const struct ndn_pkey *private_key_ndn)
 	bio = BIO_new(BIO_s_mem());
 	JUMP_IF_NULL(bio, openssl_error);
 
-	r = PEM_write_bio_PrivateKey(bio, (EVP_PKEY *) private_key_ndn, NULL, NULL, 0, NULL,
-			NULL);
+        if (password) {
+          r = PEM_write_bio_PKCS8PrivateKey (bio, (EVP_PKEY *) private_key_ndn, EVP_aes_256_cbc (), NULL, 0, NULL, password);
+        }
+        else {
+          r = PEM_write_bio_PrivateKey(bio, (EVP_PKEY *) private_key_ndn, NULL, NULL, 0, NULL, NULL);
+        }
 	if (!r)
 		goto openssl_error;
 
@@ -480,8 +492,9 @@ error:
 
 int
 read_key_pem(FILE *fp, PyObject **py_private_key_ndn,
-		PyObject **py_public_key_ndn, PyObject **py_public_key_digest,
-		int *public_key_digest_len)
+             PyObject **py_public_key_ndn, PyObject **py_public_key_digest,
+             int *public_key_digest_len,
+             char *password)
 {
         struct ndn_pkey *private_key = NULL;
 	PyObject *py_private_key = NULL, *py_public_key = NULL;
@@ -493,7 +506,7 @@ read_key_pem(FILE *fp, PyObject **py_private_key_ndn,
 	r = fgetpos(fp, &fpos);
 	JUMP_IF_NEG(r, errno_error);
 
-	private_key = (struct ndn_pkey *)PEM_read_PrivateKey(fp, NULL, NULL, NULL);
+        private_key = (struct ndn_pkey *)PEM_read_PrivateKey(fp, NULL, NULL, password);
 	if (private_key) {
 		public_only = 0;
 		goto success;
@@ -551,7 +564,8 @@ error:
 int
 put_key_pem(int is_public_only, PyObject *py_key_pem,
             PyObject **py_private_key_ndn, PyObject **py_public_key_ndn,
-            PyObject **py_public_key_digest)
+            PyObject **py_public_key_digest,
+            char *password)
 {
 	unsigned char *key_pem;
 	Py_ssize_t pem_len;
@@ -569,7 +583,7 @@ put_key_pem(int is_public_only, PyObject *py_key_pem,
 	if (is_public_only)
           key = (struct ndn_pkey*)PEM_read_bio_PUBKEY(bio, NULL, NULL, NULL);
 	else
-          key = (struct ndn_pkey*)PEM_read_bio_PrivateKey(bio, NULL, NULL, NULL);
+          key = (struct ndn_pkey*)PEM_read_bio_PrivateKey(bio, NULL, NULL, password);
 	JUMP_IF_NULL(key, openssl_error);
 
 	r = ndn_keypair(is_public_only, key, py_private_key_ndn, py_public_key_ndn);

csrc/key_utils.h
@@ -47,19 +47,21 @@ int generate_key(int length, PyObject **private_key_ndn,
 //int generate_keypair(int length, struct keypair** KP);
 
 // We use "PEM" to make things "readable" for now
-int write_key_pem_private(FILE *fp, struct ndn_pkey* private_key_ndn);
+int write_key_pem_private(FILE *fp, struct ndn_pkey* private_key_ndn, char *password);
 int write_key_pem_public(FILE *fp, struct ndn_pkey* private_key_ndn);
 
-PyObject *get_key_pem_private(const struct ndn_pkey *private_key_ndn);
+PyObject *get_key_pem_private(const struct ndn_pkey *private_key_ndn, char *password);
 PyObject *get_key_pem_public(const struct ndn_pkey *key_ndn);
 PyObject *get_key_der_private(struct ndn_pkey *private_key_ndn);
 PyObject *get_key_der_public(struct ndn_pkey *public_key_ndn);
 int read_key_pem(FILE *fp, PyObject **py_private_key_ndn,
-		PyObject **public_key_ndn, PyObject **py_public_key_digest,
-		int *public_key_digest_len);
+                 PyObject **public_key_ndn, PyObject **py_public_key_digest,
+                 int *public_key_digest_len,
+                 char *password);
 int put_key_pem(int is_public_only, PyObject *py_key_pem,
 		PyObject **py_private_key_ndn, PyObject **py_public_key_ndn,
-		PyObject **py_public_key_digest);
+		PyObject **py_public_key_digest,
+                char *password);
 int put_key_der(int is_public_only, PyObject *py_key_der,
 		PyObject **py_private_key_ndn, PyObject **py_public_key_ndn,
 		PyObject **py_public_key_digest, int *public_key_digest_len);

python/pyndn/Key.py
@@ -32,16 +32,16 @@ class Key(object):
 	def publicToDER(self):
 		return _pyndn.DER_write_key(self.ndn_data_public)
 
-	def privateToPEM(self, filename = None):
+	def privateToPEM(self, filename = None, password = None):
 		if not self.ndn_data_private:
 			raise _pyndn.NDNKeyError("Key is not private")
 
 		if filename:
 			f = open(filename, 'w')
-			_pyndn.PEM_write_key(self.ndn_data_private, file=f)
+			_pyndn.PEM_write_key(self.ndn_data_private, file=f, password = password)
 			f.close()
 		else:
-			return _pyndn.PEM_write_key(self.ndn_data_private)
+			return _pyndn.PEM_write_key(self.ndn_data_private, password = password)
 
 	def publicToPEM(self, filename = None):
 		if filename:
@@ -61,15 +61,15 @@ class Key(object):
 				_pyndn.DER_read_key(public=public)
 			return
 
-	def fromPEM(self, filename = None, private = None, public = None):
+	def fromPEM(self, filename = None, private = None, public = None, password = None):
 		if filename:
 			f = open(filename, 'r')
 			(self.ndn_data_private, self.ndn_data_public, self.publicKeyID) = \
-				_pyndn.PEM_read_key(file=f)
+				_pyndn.PEM_read_key(file=f, password = password)
 			f.close()
 		elif private:
 			(self.ndn_data_private, self.ndn_data_public, self.publicKeyID) = \
-				_pyndn.PEM_read_key(private=private)
+				_pyndn.PEM_read_key(private=private, password = password)
 		elif public:
 			(self.ndn_data_private, self.ndn_data_public, self.publicKeyID) = \
 				_pyndn.PEM_read_key(public=public)
@@ -81,9 +81,9 @@ class Key(object):
                 return key
 
         @staticmethod
-        def createFromPEM (filename = None, private = None, public = None):
+        def createFromPEM (filename = None, private = None, public = None, password = None):
                 key = Key ()
-                key.fromPEM (filename, private, public)
+                key.fromPEM (filename, private, public, password)
                 return key
 
 # plus library helper functions to generate and serialize keys?